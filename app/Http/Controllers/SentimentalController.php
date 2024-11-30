4b02059a88d07a58ede272fd73776c9cb14d60f5
Rich1068 <rs106848@gmail.com>
2024-11-30 21:13:32 +0800

    added color

--- app/Http/Controllers/SentimentalController.php
+++ app/Http/Controllers/SentimentalController.php
@@ -12,11 +12,14 @@ class SentimentalController extends Controller
     {
         return view('sentimental');
     }
+
     public function analyse(Request $request): View
     {
         $analyzer = new Analyzer();
+        //scan the input
         $output_text = $analyzer->getSentiment($request->text_to_analyze);
            $mood = '';
+           //determining the mood
            if($output_text['neg'] > 0 && $output_text['neg'] < 0.49){
                $mood = 'Somewhat Negative ';
            }
@@ -37,8 +40,32 @@ class SentimentalController extends Controller
            elseif($output_text['pos'] > 0.49){
                $mood = 'Mostly positive';
            }
-           //dd('Negative: ' . $output_text['neg'] . ' Positive: ' . $output_text['pos'] . ' Neutral: '. $output_text['neu']);
-           return view('sentimental')->with('text',$mood);
-       
+
+        $text = $request->text_to_analyze;
+        if (!empty($output_text['pos_words'])) {
+            foreach ($output_text['pos_words'] as $word) {
+                $text = preg_replace(
+                    "/\b" . preg_quote($word, '/') . "\b/i",
+                    "<span style='color: green; font-weight: bold;'>$word</span>",
+                    $text
+                ) ?? $text;
+            }
+        }
+    
+        // Highlight negative words
+        if (!empty($output_text['neg_words'])) {
+            foreach ($output_text['neg_words'] as $word) {
+                $text = preg_replace(
+                    "/\b" . preg_quote($word, '/') . "\b/i",
+                    "<span style='color: red; font-weight: bold;'>$word</span>",
+                    $text
+                ) ?? $text;
+            }
+        }
+           return view('sentimental')->with([
+            'mood' => $mood,
+            'scores' => $output_text,
+            'highlighted_text' => $text,
+        ]);
     }
 }

ebb2cb6114806be17096ded1de22cb0d2b328e60
Rich1068 <rs106848@gmail.com>
2024-12-06 01:42:35 +0800

    new seeder for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserHistory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,17 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'superadmin1@gmail.com',
             'password' => '12345678'
+        ]);
+        UserHistory::factory()->create([
+            'user_id' => 1,
+            'input_text' => 'This is a sample input text for sentiment analysis.',
+            'file_path' => 'uploads/text.txt',
+            'negative_score' => 0.1,
+            'neutral_score' => 0.6,
+            'positive_score' => 0.3,
+            'result' => 'Neutral',
+            'created_at' => now()->subDays(31),
+            'updated_at' => now()->subDays(31),
         ]);
     }
 }
